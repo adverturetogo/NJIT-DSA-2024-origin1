f276ec305c70887ebbc035d103415e554884a524
adverturetogo <2323100321@qq.com>
2024-02-28 10:48:27 +0800

    src/main/java/oy/tol/tra/SSN.java

--- 00-init/src/main/java/oy/tol/tra/SSN.java
+++ 00-init/src/main/java/oy/tol/tra/SSN.java
@@ -100,7 +100,8 @@ public class SSN {
                String personNumberString = ssn.substring(PERSON_CODE_INDEX_START,
                      PERSON_CODE_INDEX_START + PERSON_CODE_LENGTH);
                // Next checking the three digit number after the century separator.
-               Integer personNumber = Integer.parseInt(personNumberString);
+               String numericString = personNumberString.replaceAll("[^0-9]", "");
+               Integer personNumber = Integer.parseInt(numericString);
                if (personNumber > 0) {
                   // If it was a positive integer, then calculate the checksum.
                   StringBuilder builder = new StringBuilder();
@@ -108,7 +109,8 @@ public class SSN {
                   builder.append(ssn.substring(4, DATE_PART_END_SUBSTRING_INDEX));
                   builder.append(personNumberString);
                   String checkSumString = builder.toString();
-                  Long checkSum = Long.parseLong(checkSumString) % CHECK_CODE_DIVIDER;
+                  String ads =checkSumString.replaceAll("[^0-9]", "");
+                  Long checkSum = Long.parseLong(ads) % CHECK_CODE_DIVIDER;
                   if (CHECKCHARS.charAt(checkSum.intValue()) == ssn.charAt(CHECKCODE_INDEX)) {
                      if (personNumber > 2 && personNumber < 900) {
                         // Checksum was correct for a real SSN.
