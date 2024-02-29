0d0f95ed47e887289357995d77f4498ab6c2f5bc
adverturetogo <2323100321@qq.com>
2024-02-29 11:38:38 +0800

    po

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -1,5 +1,5 @@
 package oy.tol.tra;
-
+import java.util.Arrays;;
 /**
  * A simple array of student grades to be used in testing
  * misbehaving algorithm for reversing the array.
@@ -16,6 +16,7 @@ public class Grades {
       this.grades = new Integer [grades.length];
       for (int counter = 0; counter < grades.length; counter++) {
          this.grades[counter] = grades[counter];
+         
       }
    }
 
@@ -31,18 +32,39 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
-      int i = 0;
-      while (i <= grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length-i-1];
-         grades[grades.length-i-1] = temp;
-         i++;
-     }
+      int left = 0;
+      int right = grades.length - 1;
+      while (left < right) {
+          int temp = grades[left];
+          grades[left] = grades[right];
+          grades[right] = temp;
+          left++;
+          right--;
+      }
+      
    }
 
    /**
     * Sorts the array to ascending order.
     */
+    
+    public static void insertionSort(Integer[] grades2) {
+      if (grades2 == null || grades2.length <= 1) {
+          return;
+      }
+
+      for (int i = 1; i < grades2.length; i++) {
+          int current = grades2[i];
+          int j = i - 1;
+
+          while (j >= 0 && grades2[j] > current) {
+              grades2[j + 1] = grades2[j];
+              j--;
+          }
+
+          grades2[j + 1] = current;
+      }
+  }
    public void sort() {
       /* TODO:
        1. Edit the test data files to see if the sort() really works or not.
@@ -52,15 +74,8 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
       */
-      int i = grades.length-1;
-      while (i > 0) {
-         if (grades[i] < grades[i-1]) {
-            int tmp = grades[i];
-            grades[i] = grades[i-1];
-            grades[i-1] = tmp;
-         }
-         i--;
-      }
+      insertionSort(grades);
+
    }
 
    /**
