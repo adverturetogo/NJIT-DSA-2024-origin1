040c558ab1c5b457159389d130687183c05e83c0
adverturetogo <2323100321@qq.com>
2024-04-02 19:57:57 +0800

    new task3

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -3,26 +3,6 @@ package oy.tol.tra;
 import java.util.Arrays;
 
 public  class Algorithms{
-    public static int  binarySearch2(int aValue,Integer[]arr,int from,int toIndex)
-    {
-      if(from<=toIndex)
-      {
-        int mid=from+(toIndex-from)/2;
-        if(arr[mid]<aValue)
-        {
-            return binarySearch(aValue, arr, mid+1, toIndex);
-        }
-        else if(arr[mid]==aValue)
-        {
-            return mid;
-        }
-        else{
-            return binarySearch(aValue, arr, from, mid-1);
-        }
-      }    
-      return -1;      
-    }
-   
     public static <T extends Comparable<T>> int binarySearch(T target, T[] arr, int from, int toIndex) {
         if (from <= toIndex) {
             int mid = from + (toIndex - from) / 2;
@@ -40,44 +20,34 @@ public  class Algorithms{
         
         return -1;
     }
-    
-    
-    public static  <T extends Comparable<T>> void sort(T[]arr)
-    {
-        QuickSort(arr, 0, arr.length-1);
-    }
-   
-    
-    
-    
-
-
-    private static <T extends Comparable<T>>  void QuickSort(T[] arr, int left, int right) {
-        if (left < right) {
-            int partitionIndex = partition(arr, left, right);
-            QuickSort(arr, left, partitionIndex - 1);
-            QuickSort(arr, partitionIndex + 1, right);
+    public static <T extends Comparable<T>> void sort(T [] array) {
+        if (array == null || array.length <= 1) {
+            return;
         }
-    }
-
-    private static <T extends Comparable<T>>  int partition(T[] arr, int left, int right) {
-        T pivot = arr[right];
-        int i = left - 1;
-
-        for (int j = left; j < right; j++) {
-            if  (arr[j].compareTo(pivot) <= 0) {
-                i++;
-                swap(arr, i, j);
+  
+        for (int i = 1; i < array.length; i++) {
+            T current = array[i];
+            int j = i - 1;
+  
+            while (j >= 0 && array[j] .compareTo(current) >0) {
+                array[j + 1] = array[j];
+                j--;
             }
+  
+            array[j + 1] = current;
         }
-
-        swap(arr, i + 1, right);
-        return i + 1;
     }
-
-    private static <T extends Comparable<T>>  void swap(T[] arr, int i, int j) {
-        T temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+     // ...
+     public static <T> void reverse(T [] array) {
+        int left = 0;
+        int right = array.length - 1;
+        
+        while (left < right) {
+            T temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+     }
+}
   }
